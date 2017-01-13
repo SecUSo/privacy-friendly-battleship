fe836f5276fc939f953346c8801e63b2b2806489
Ali Kalsen <a.kalsen@gmx.de>
2017-01-13 14:52:08 +0100

    First Implementation of MainActivity

--- app/src/main/java/org/secuso/privacyfriendlybattleships/MainActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlybattleships/MainActivity.java
@@ -5,31 +5,34 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends BaseActivity {
 
+    private SharedPreferences preferences = null;
+    private ViewPager viewPager = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setupPreferences();
 
-        // Use the a button to display the welcome screen
-        Button b = (Button) findViewById(R.id.button_welcomedialog);
-        if(b != null) {
-            b.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    WelcomeDialog welcomeDialog = new WelcomeDialog();
-                    welcomeDialog.show(getFragmentManager(), "WelcomeDialog");
-                }
-            });
+        // Show the welcome dialog only once after the app has been started
+        if (isFirstAppStart()) {
+            showWelcomeDialog();
+            setAppStarted();
         }
 
-        overridePendingTransition(0, 0);
+        // Initialize the main page
+        setContentView(R.layout.activity_main);
+        setupViewPager();
     }
 
     @Override
@@ -37,8 +40,28 @@ public class MainActivity extends BaseActivity {
         return R.id.nav_example;
     }
 
+    public void setupViewPager(){
+        //TODO: Implement this method
+    }
+
+    private void showWelcomeDialog() {
+        new WelcomeDialog().show(getFragmentManager(), WelcomeDialog.class.getSimpleName());
+    }
+
+    private void setupPreferences() {
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+    }
+
+    private void setAppStarted() {
+        preferences.edit().putBoolean(Constants.FIRST_APP_START, false).commit();
+    }
+
+    private boolean isFirstAppStart() {
+        return preferences.getBoolean(Constants.FIRST_APP_START, true);
+    }
 
     public static class WelcomeDialog extends DialogFragment {
+
 
         @Override
         public void onAttach(Activity activity) {
@@ -50,24 +73,30 @@ public class MainActivity extends BaseActivity {
 
             LayoutInflater i = getActivity().getLayoutInflater();
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
             builder.setView(i.inflate(R.layout.welcome_dialog, null));
             builder.setIcon(R.mipmap.icon);
-            builder.setTitle(getActivity().getString(R.string.welcome));
-            builder.setPositiveButton(getActivity().getString(R.string.okay), null);
-            builder.setNegativeButton(getActivity().getString(R.string.viewhelp), new DialogInterface.OnClickListener() {
+            builder.setTitle(getActivity().getString(R.string.welcome_title));
+            builder.setPositiveButton(getActivity().getString(R.string.button_ok), null);
+            builder.setNegativeButton(getActivity().getString(R.string.button_help), new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
-                    ((MainActivity)getActivity()).goToNavigationItem(R.id.nav_help);
+                    Intent intent = new Intent(getActivity(), HelpActivity.class);
+                    intent.putExtra(HelpActivity.EXTRA_SHOW_FRAGMENT, HelpActivity.HelpFragment.class.getName());
+                    intent.putExtra(HelpActivity.EXTRA_NO_HEADERS, true);
+                    startActivity(intent);
                 }
             });
-
             return builder.create();
         }
     }
 
+}
+
+
     public void onClick(View view) {
         switch(view.getId()) {
-            // do something with all these buttons?
+            case R.arrowLeft
             default:
         }
     }
