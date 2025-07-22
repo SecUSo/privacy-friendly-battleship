SecUSo/privacy-friendly-2048

Fixes copy-paste error regarding package

app/src/main/java/org/secuso/privacyfriendly2048/GoodbyeGoogleHelper.kt
@@ -1,4 +1,4 @@
-package org.secuso.privacyfriendlyboardgameclock
+package org.secuso.privacyfriendly2048;
 
 import android.content.Context
 import android.graphics.text.LineBreaker

SecUSo/privacy-friendly-battleship

app/src/main/java/org/secuso/privacyfriendlybattleship/GoodbyeGoogleHelper.kt
@@ -1,4 +1,4 @@
-package org.secuso.privacyfriendlyboardgameclock
+package org.secuso.privacyfriendlybattleship;
 
 import android.content.Context
 import android.graphics.text.LineBreaker